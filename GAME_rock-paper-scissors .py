206affa85f1793d592eca64ed7c09a5875c1b76c
delta1ox <surajvinaykumar1999@gmail.com>
2024-01-16 21:36:08 -0800

    First commit

--- GAME_rock-paper-scissors .py
+++ GAME_rock-paper-scissors .py
@@ -1,27 +1,33 @@
 import random
 
 options = ("rock", "paper", "scissors") #fixed data so using a tuple
-player = None
-computer = random.choice(options) # here computer is a variable which will
-                                  # choose the items from the tuple "options"
+playing = True
 
-while player not in options:
-    print("INVALID")
-    player = input("Enter a choice (Rock, paper, scissors)---->>>>   ")
+while playing:
+    player = None
+    computer = random.choice(options) # here computer is a variable which will
+                                    # choose the items from the tuple "options"
 
+    while player not in options:
+        print("INVALID")
+        player = input("Enter a choice (Rock, paper, scissors)---->>>>   ")
 
-print(f"Player: {player}")
-print(f"Computer: {computer}")
+        print(f"Player: {player}")
+        print(f"Computer: {computer}")
 
-if player == computer:
-    print("It's a tie!")
-elif player == "rock" and computer == "scissors":
-    print("you win!")
-elif player == "paper" and computer == "rock":
-    print("you win!")
-elif player == "scissors" and computer == "paper":
-    print("you win!")
+        if player == computer:
+            print("It's a tie!")
+        elif player == "rock" and computer == "scissors":
+            print("you win!")
+        elif player == "paper" and computer == "rock":
+            print("you win!")
+        elif player == "scissors" and computer == "paper":
+            print("you win!")
 
-else:
-    print("you loose")
-     
+        else:
+            print("you loose")
+
+        if not input ("Play again?  (y/n)--->  ").lower()=="y":
+            playing = False
+print("Thanks for playing! ")
+        
